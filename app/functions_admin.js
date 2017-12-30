2129a5a65fcbd62a580c8433c44aaf1cad47aa17
Harshwardhan619 <hmourya2001@gmail.com>
2017-12-30 18:12:53 +0530

    ADDED bohot saari things for forking

---
+++ app/functions_admin.js
@@ -0,0 +1,333 @@
+// Admin routes functions
+
+
+// load all the things we need.
+var functions = require('./functions');
+var others = require('./others');
+
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var connection = mysql.createConnection(dbconfig.connection);
+
+connection.query('USE ' + dbconfig.database);
+
+// ========================================================
+
+var schedule = require('node-schedule');
+
+var express  = require('express');
+var app = express();
+
+// ================== DAILY AUCTION TIME ===================
+
+var DAST = "15:00:00";
+var DAET = "20:00:00";
+
+// =========================================================
+
+module.exports = {
+
+	//================================================================================
+    //======================= ADMIN FUNCTIONS ========================================
+    //================================================================================
+
+    //Handle POST request to add a new working location 
+    add_new_location: function(req, res){
+    	var city = req.body.city;
+    	var city = city.toUpperCase(); // Change CASE to uppercase for handling case-coflicts while comparing
+
+    	connection.query("SELECT * FROM location WHERE city = ?",[city], function(err, rows) {
+            if (err){
+                throw err;
+            }
+            else if (rows.length) {
+                req.send("That Location already exists");
+            } else {
+            	var state = req.body.state;
+               	var insertQuery = "INSERT INTO location ( city, state, dealer_count, equipment_count) values (?,?,?,?)";
+                // insert with initial deal_count, equipment_count to zero
+               	connection.query(insertQuery, [city, state, 0, 0],function (err, rows) {
+				    if (err) throw err;
+				    console.log("New Location Added");
+				});
+
+				res.send("New Location Added");
+            }
+            return ;
+    	});
+    },
+
+    // Show existing locations
+    existing_location: function(req,res){
+    	connection.query("SELECT * FROM location", function(err, rows){
+    		if (err){
+                throw err;
+    		}
+            // if there is no locations
+    		else if (!rows.length) {
+    			res.send("Please add a location to view"); 
+    		}
+    		else {
+    			res.send(rows);
+    		}
+    	});
+    },
+
+    // Show all the sub-Admin working under the particuler logged in admin.
+    existing_user: function(req,res){
+        //query for searching from tree like stucture.
+        selectquery = "select * from ( select  * FROM (select * from admin order by boss_id, id) products_sorted, (select @pv := ?) initialisation where find_in_set(boss_id, @pv) > 0 and @pv := concat(@pv, ',', id) ) a, account b where a.id = b.id";
+    	connection.query(selectquery, [req.session.user], function(err, rows){
+    		if (err){
+                throw err;
+    		}
+    		else if (!rows.length) {
+    			req.send("No User is working Under you");
+    		}
+    		else {
+    			res.send(rows);
+    		}
+    	});
+    },
+
+    // Handle get request and give all locations for drop-down for adding new admin
+    add_new_admin: function(req,res){
+    	connection.query("SELECT sno, city FROM location", function(err, rows){
+    		if (err){
+                throw err;
+    		}
+    		else if (!rows.length) {
+    			res.send('Please add a location to add a new admin');
+    		}
+    		else {
+    			res.send(rows);
+    		}
+    	});
+    },
+
+    // Add a new admin under an admin and send a mail for resetting the password;
+    add_new_admin_post_form: function(req,res){
+    	var data = req.body;
+        // if 
+    	connection.query("SELECT * FROM account WHERE mobile = ?",[data.mobile], function(err, rows) {
+            if (err)
+                throw err;
+            if (rows.length) {
+                req.send("That mobile is already taken");
+            } else {
+   	            // if there is no user with that mobile number then create the user
+                var insertQuery1 = "INSERT INTO account ( mobile, email, mobile, wallet, address) values (?,?,?,?,?,?,?)";
+                var category = 3;
+                var wallet = 0;
+	            connection.query(insertQuery1,[data.mobile, data.email, data.mobile, wallet, data.address], function(err, rows) {
+                    console.log(rows);
+	            	if (err){
+                		throw err;
+    				}
+    				else {
+                    // insert data into admin table 
+	            	var insertQuery2 = "INSERT INTO admin ( id, location, boss_id) values (?,?,?)";
+	            	connection.query(insertQuery2,[rows.insertId, data.location, req.session.user]);
+                    connection.query("SELECT * from account WHERE mobile = ?", [data.mobile], function(err, rows){
+                        // generate mail to send to the email for reseting password
+                        others.generate_mail(req, rows);
+                        res.redirect('/profile');
+                    });
+	            	}
+	            });
+            }
+        });
+    },
+    
+    // Handle post request to handle addition of standard equipment.
+    add_new_equipment_post_form: function(req,res){
+        data = req.body;
+        var name = {};
+        name.brand = data.brand;
+        delete data["brand"];
+        name.model = data.model;
+        delete data["model"];
+        name.varient = data.varient;
+        delete data["varient"];
+        var description = data.description;
+        delete data["description"];
+        data =JSON.stringify(data);
+
+        // Check if equipment doesn't already exist.
+        connection.query("SELECT * FROM std_equipment WHERE (brand = ? AND model = ? AND varient = ?)",[name.brand, name.model, name.varient], function(err, rows) {
+            if (err){
+                throw err;
+            }
+            else if (rows.length) {
+                req.send("That Equipment already exists");
+            } 
+            else {
+                var insertQuery = "INSERT INTO std_equipment (brand, model, varient, details, description, search_count, view_count, auction_count) values (?,?,?,?,?,?,?,?)";
+                connection.query(insertQuery, [name.brand, name.model, name.varient, data, description, 0, 0, 0],function (err, rows) {
+                    if (err) throw err;
+                    else {
+                        res.send("Added new Equipment");
+                    }
+                });
+            }
+        });
+    },
+
+    add_new_equipment_by_csv: function(req, res){
+        //
+        //
+        //
+        //
+        //
+    },
+
+    // add new auction of D->C andd C->C type
+    add_new_auction_post_form: function(req, res){
+        var data = req.body;
+        console.log(data);
+        //Check if auction_timings does'nt clashes with already scheduled auction.
+        var selectquery = "Select * from auction WHERE ( (type = ?) AND ( ( (start_time <= ? ) AND (? <= end_time) ) OR ( (start_time <= ? ) AND (? <= end_time) ) OR ( (? <= start_time) AND (? >= end_time) ) ) )"
+        
+        connection.query(selectquery,[2, data.start_time, data.start_time, data.end_time, data.end_time, data.start_time, data.end_time], function(err, rows){
+            if(err) throw err;
+            else if(rows.length){
+                res.send('That Auction Timings clashes with another Auction');
+                console.log(rows);
+            }
+            else{
+                // Insert to auction table
+                var insertQuery = "INSERT INTO auction (start_time, end_time, type) values (?,?,?)";
+                connection.query(insertQuery, [data.start_time, data.end_time, 2],function (err, rows) {
+                    if (err) throw err;
+                    else {
+
+                        console.log("New C->C + D->C Auction Added with start_time: " + data.start_time + " and end_time: " + data.end_time);
+
+                        var upcoming_auction = functions.next_upcoming_auction(2);
+
+                        if(!upcoming_auction){
+                            //console.log("no_users");
+                            res.send("New Auction Added");
+                        }
+                        else {
+                            // Allocate cars to next_upcoming auction.
+                            connection.query("UPDATE all_equipment SET auction = ? WHERE (auction_para = ?)", [upcoming_auction, 2], function(err, rows){
+                                console.log("ALLOTED AUCTIONS");
+                                res.send("New Auction Added");
+                            });
+                            // functions.insert_object(2);
+                            // res.send("New Auction Added");
+                        }
+                    }
+                });
+            }
+        });
+    },
+
+
+    // Add enquiry and send it to next higher admin
+    enquiry_form_post_form : function(req, res){
+        var data = req.body;
+        var user = req.session.user;
+        var category = req.session.category;
+
+        // Select boss_id from admin
+        selectquery = "SELECT boss_id from admin where id = ?"
+        connection.query(selectquery, [user], function(err, rows){
+            console.log(rows);
+
+            connection.query("INSERT INTO enquiry (sender_id, reciever_id, description, subject) values (?,?,?,?)",[user, rows[0].boss_id, data.description, data.subject], function(err, rows){
+                if(err) throw err;
+                else{
+                    res.send("your enquiry is submitted");
+                }
+            } );
+        });
+    },
+
+
+
+    //Schedule for scheduling daily auction C->D auctions
+    schedule_auction : function(){
+        // config things that we need
+        var rule = new schedule.RecurrenceRule();
+        rule.dayOfWeek = [new schedule.Range(0, 6)];
+        rule.hour = 1;
+        rule.minute = 0;
+        today_date =  new Date();
+
+        // convert date time to correct format.
+        var start_time = today_date.getFullYear() + '-' + today_date.getMonth() + '-' + today_date.getDate() + 'T' + DAST;
+        var end_time = today_date.getFullYear() + '-' + today_date.getMonth() + '-' + today_date.getDate() + 'T' + DAET;
+
+        console.log('node-schedule is running');
+
+        //schedule job at 1:00AM daily to schdule a C->D auction
+        var j = schedule.scheduleJob(rule, function(){
+            addquery = "INSERT INTO auction (start_time, end_time, type) values (?,?,?)"
+            connection.query(addquery, [start_time, end_time, 1], function(err, rows){
+                if (err) throw err;
+                else{
+                    console.log("New Auction Added");
+                    //call this function to add at the end of auction
+                    module.exports.end_schedule(rows.insertId, end_time);
+                }
+            });
+        });
+    },
+
+    // Things to do at the end of the auction
+    end_schedule : function(auction_id, end_time){
+
+        var j = schedule.scheduleJob(end_time, function(){
+            //insert all data into auction_bid after the auction
+
+            selectquery = "INSERT INTO auction_object (id, equip_id, auction_id, buyer_id, maxi_bid, time, mini_bid) (select a.*, b.mini_bid from (SELECT b.* from (SELECT equip_id, MAX(bid_price) AS bid_price FROM bids where auction_id = ? GROUP BY equip_id) a, bids b where a.equip_id = b.equip_id AND a.bid_price = b.bid_price and auction_id = ?) a, all_equipment b where a.equip_id = b.id OR (b.mini_bid = b.next_bid AND auction = ?) )";
+
+
+            connection.query(selectquery, [auction_id, auction_id, auction_id], function(err, rows){
+                if(err) throw err;
+                else {
+                    console.log("Insert in auction_aubject completed");
+                }
+            });
+
+            // Update action_para to 4 if someone has bidded else change it to 0
+            updatequery = "update all_equipment set auction_para = CASE WHEN mini_bid = next_bid THEN '0' ELSE '4' END WHERE auction = ?"
+            connection.query(update,)
+
+            res.end("Auction Ended", [auction_id], function(err, rows){
+                if(err) throw err;
+                else {
+                    console.log("updated auction_para");
+                }
+            });
+
+        });
+    },
+
+    change_time : function (req, res){
+    	DAST = req.body.start_time;   //this is type = "time"
+    	DAET = req.body.end_time;
+    },
+
+    // see all pending delar verifications
+    pending_request: function(req, res){
+        // join for selecting data from both tables
+        connection.query(selectquery = "SELECT * (Select * FROM dealer where is_authenthcated = ?) a, account b where a.id = b.id",[0], function(err, rows){
+            if (err) throw err;
+            else{
+                res.send(rows);
+            }
+        });
+    },
+
+    // change dealer verfication status when verified
+    verified : function(req, res){
+        connection.query("update dealer set is_authenthcated = ? where id = ?", [1, req.body.id], function(req, res){
+            if(err) throw err;
+            else res.send("Dealer is verified");
+        });
+    }
+
+}
--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,27 @@
 // app/routes.js
 
-var functions = require('./functions')
+//load all the things needed.
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var connection = mysql.createConnection(dbconfig.connection);
+
+connection.query('USE ' + dbconfig.database);
 
 
-// var dbconfig = require('./../config/database');
-// var connection = mysql.createConnection(dbconfig.connection);
+var express  = require('express');
+var app = express();
 
-// connection.query('USE ' + dbconfig.database);
+// import functions from other files.
+var functions = require('./functions');
+var functions_admin = require('./functions_admin');
+var functions_dealer = require('./functions_dealer');
+
 // ==========================================
 module.exports = function(app, passport) {
 
     // HOME PAGE
     app.get('/',functions.isLoggedInfunc, function(req, res) {
-        console.log(req.session);
+        console.log("Logged in with id: " + req.session.user);
         res.send("Hello, Welcome");
     });
 
@@ -23,7 +32,6 @@ module.exports = function(app, passport) {
     // process the LOGIN form
     app.post('/login', function(req, res, next){
         passport.authenticate('local-login', function (err, user, info) {
-
             //this function is called when LocalStrategy returns done function with parameters
 
             //if any error , throw error to default error handler
@@ -67,7 +75,7 @@ module.exports = function(app, passport) {
     }));
 
     // PROFILE SECTION =====================
-    app.get('/profile', functions.isLoggedInfunc, functions.profilefunc); // issLoggedIn verifies that user is authenticated
+    app.get('/profile/:id', functions.isLoggedInfunc, functions.profilefunc); // issLoggedIn verifies that user is authenticated
 
     // LOGOUT ==============================
     app.get('/logout', functions.logoutfunc);
@@ -86,78 +94,92 @@ module.exports = function(app, passport) {
     //POST for password reset and if token hasn't expired, the password of user is reset.
     app.post('/reset/:token', functions.reset_pass_post_form);
 
+    app.post('/add_to_likes', functions.isLoggedInfunc, dealer_user_access, functions.add_to_likes);
+
+    app.post('/search_suggestions', functions.isLoggedInfunc, dealer_user_access, functions.search_suggestions);
+
+    app.post('/search', functions.isLoggedInfunc, dealer_user_access, functions.search);
+
+    app.get('/my_likes', functions.isLoggedInfunc, dealer_user_access, functions.my_likes);    
+
+     app.get('/show_car/:id', functions.isLoggedInfunc, functions.show_car);
 
     //================================================================================
     //======================= ADMIN ROUTES ===========================================
     //================================================================================
 
-    app.get('/new_location', functions.isLoggedInfunc, admin_access, functions.new_location);
-    app.post('/new_location', functions.isLoggedInfunc, admin_access, functions.add_new_location);
-    app.get('/existing_location', functions.isLoggedInfunc, admin_access, functions.existing_location);
-    app.get('/existing_user', functions.isLoggedInfunc, admin_access, functions.existing_user);
-    app.get('/add_new_admin', functions.isLoggedInfunc, admin_access, functions.add_new_admin);
-    app.post('/add_new_admin', functions.isLoggedInfunc, admin_access, functions.add_new_admin_post_form);
-    app.get('/add_new_equipment', functions.isLoggedInfunc, admin_access, functions.add_new_equipment);
-    app.post('/add_new_equipment', functions.isLoggedInfunc, admin_access, functions.add_new_equipment_post_form);
-    app.get('/add_new_auction', functions.isLoggedInfunc, admin_access, functions.add_new_auction);
-    app.post('/add_new_auction', functions.isLoggedInfunc, admin_access, functions.add_new_auction_post_form);
-    app.get('/existing_dealers', functions.isLoggedInfunc,  functions.existing_dealers);    //common for admin and dealer
-    app.post('/existing_dealers', functions.isLoggedInfunc, functions.existing_dealers);
-    app.get('/enquiry_form', functions.isLoggedInfunc, admin_access, functions.enquiry_form);
-    app.post('/enquiry_form', functions.isLoggedInfunc, admin_access, functions.enquiry_form_post_form);
 
+    app.post('/admin/new_location', functions.isLoggedInfunc, admin_access, functions_admin.add_new_location);
+    app.get('/admin/existing_location', functions.isLoggedInfunc, admin_access, functions_admin.existing_location);
+    app.get('/admin/existing_user', functions.isLoggedInfunc, admin_access, functions_admin.existing_user);
+    app.get('/admin/add_new_admin', functions.isLoggedInfunc, admin_access, functions_admin.add_new_admin);
+    app.post('/admin/add_new_admin', functions.isLoggedInfunc, admin_access, functions_admin.add_new_admin_post_form);
+    app.post('/admin/add_new_equipment', functions.isLoggedInfunc, admin_access, functions_admin.add_new_equipment_post_form);
+    app.post('/admin/add_new_auction', functions.isLoggedInfunc, admin_access, functions_admin.add_new_auction_post_form);
+
+    app.get('/existing_dealers', functions.isLoggedInfunc, functions.existing_dealers);    //common for admin and dealer
+
+    app.post('/admin/enquiry_form', functions.isLoggedInfunc, admin_access, functions_admin.enquiry_form_post_form);
+    app.post('/admin/change_time', functions.isLoggedInfunc, admin_access, functions_admin.change_time);
+    app.get('/pending_request', functions.isLoggedInfunc, admin_access, functions_admin.pending_request);
+    app.post('/verified', functions.isLoggedInfunc, admin_access, functions_admin.verified);
 
     //================================================================================
-    //======================= DEALER ROUTES ==========================================
+    //======================= DEALER ROUTES + USER ROUTES ============================
     //================================================================================
 
-    app.get('/complete_profile', functions.isLoggedInfunc, dealer_access, functions.complete_profile);
-    app.post('/complete_profile', functions.isLoggedInfunc,  dealer_access, functions.complete_profile_post_form);
-    app.get('/add_car', functions.isLoggedInfunc, dealer_access, functions.add_car); 
-    app.post('/add_car', functions.isLoggedInfunc, dealer_access,  functions.add_car_post_form);
-    app.get('/dealer_my_equipment', functions.isLoggedInfunc, dealer_access,  functions.dealer_my_equipment); 
-    app.post('/change_auction_status', functions.isLoggedInfunc, dealer_access,  functions.change_auction_status);
-    app.get('/dealer_purchase', functions.isLoggedInfunc, dealer_access,  functions.dealer_purchase);
-    app.post('/dealer_purchase',functions.isLoggedInfunc, dealer_access,  functions.dealer_purchase_post_form);
-    app.get('/dealer_sell', functions.isLoggedInfunc, dealer_access,  functions.dealer_sell);
-    app.post('/dealer_sell', functions.isLoggedInfunc, dealer_access,  functions.dealer_sell_post_form);
-    app.get('/next_auction', functions.isLoggedInfunc, dealer_access,  functions.next_auction);
-    //app.post('/next_auction',functions.isLoggedInfunc, functions.dashboard);
+    app.get('/dealer/complete_profile', functions.isLoggedInfunc, dealer_access, functions_dealer.complete_profile);
+    app.post('/dealer/complete_profile', functions.isLoggedInfunc,  dealer_access, functions_dealer.complete_profile_post_form);
+    app.get('/add_car', functions.isLoggedInfunc, dealer_user_access, functions.add_car); 
+    app.post('/add_car', functions.isLoggedInfunc, dealer_user_access,  functions.add_car_post_form);
+    app.get('/dealer_my_equipment', functions.isLoggedInfunc, dealer_user_access,  functions.dealer_my_equipment); 
+    app.post('/change_auction_status', functions.isLoggedInfunc, dealer_user_access,  functions.change_auction_status);
+    app.get('/dealer/dealer_purchase', functions.isLoggedInfunc, dealer_access,  functions_dealer.dealer_purchase);
+    app.post('/dealer/dealer_purchase',functions.isLoggedInfunc, dealer_access,  functions_dealer.dealer_purchase_post_form);
+    app.get('/dealer/dealer_sell', functions.isLoggedInfunc, dealer_access,  functions_dealer.dealer_sell);
+    app.post('/dealer/dealer_sell', functions.isLoggedInfunc, dealer_access,  functions_dealer.dealer_sell_post_form);
+    app.get('/dealer/sell_lead', functions.isLoggedInfunc, dealer_access, functions_dealer.sell_lead);
+    app.post('/dealer/schedule', functions.isLoggedInfunc, dealer_access, functions_dealer.schedule);
+    app.get('dealer//my_schedule', functions.isLoggedInfunc, dealer_access, functions_dealer.my_schedule);
+
+    app.get('/next_auction', functions.isLoggedInfunc, dealer_user_access,  functions.next_auction);
+    app.get('/my_bids', functions.isLoggedInfunc, dealer_user_access,  functions.my_bids);
 
 
-    app.get('/notify', functions.isLoggedInfunc, function(req, res){
-        console.log(req.session);
-        res.send(req.session);
-    });
+    //app.get('/deal', 
     //================================================================================
     //======================== General Routes ========================================
     //================================================================================
 
-    app.post('/add_new_bid', functions.isLoggedInfunc, functions.add_new_bid);
+    app.post('/add_new_bid', functions.isLoggedInfunc,dealer_user_access, functions.add_new_bid);
 
 
+    // // TEMPORARY routes =================================================================
 
+    app.get('/temp/:data', function (req, res){
+        // for temporary use
 
-    
-
+        console.log(req.params);
+        res.send(req.params);
+    });
+    // app.post('/temp', function(req, res){
+    //     console.log(req.body);
+    //     res.send(req.body);
+    // });
 }
 
 
 var dealer_access = function access(req,res,next){
-           
-            if(req.session.category==2)
-                return next();
-            return res.render("Profiles/dealer/error.ejs");
-    }
+    if(req.session.category==2) return next();
+    return res.render("Profiles/dealer/error.ejs");
+}
 
 var admin_access = function access(req,res,next){
-           
-            if(req.session.category==3)
-                return next();
-            return res.render("Profiles/dealer/error.ejs");
-    }    
-var user_access = function access(req,res,next){
-        
-            if(req.session.category==1)
-                return next();
-            return res.render("Profiles/dealer/error.ejs");    }    
+    if(req.session.category==3) return next();
+    return res.render("Profiles/dealer/error.ejs");
+}
+
+var dealer_user_access = function access(req,res,next){
+    if(req.session.category==1 || req.session.category ==2) return next();
+    return res.render("Profiles/dealer/error.ejs");
+}    
